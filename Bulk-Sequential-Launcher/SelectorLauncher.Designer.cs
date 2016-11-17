be22392a7007e494c168c3b8756a0f06ffef1ba1
Nils Andrey Tellería Martínez <nils.telleria@datys.cu>
2016-11-17 02:57:27 -0500

    Primera versión que funciona bien.

---
+++ Bulk-Sequential-Launcher/SelectorLauncher.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Bulk_Sequential_Launcher
+{
+    partial class SelectorLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.txParameters = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnCheckAll = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(449, 311);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 76);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(523, 229);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // txParameters
+            // 
+            this.txParameters.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txParameters.Location = new System.Drawing.Point(82, 313);
+            this.txParameters.Name = "txParameters";
+            this.txParameters.Size = new System.Drawing.Size(361, 20);
+            this.txParameters.TabIndex = 2;
+            this.txParameters.Text = "/quiet";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Parameters";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "&Uncheck all";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnCheckAll
+            // 
+            this.btnCheckAll.Location = new System.Drawing.Point(93, 38);
+            this.btnCheckAll.Name = "btnCheckAll";
+            this.btnCheckAll.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckAll.TabIndex = 5;
+            this.btnCheckAll.Text = "&Check all";
+            this.btnCheckAll.UseVisualStyleBackColor = true;
+            this.btnCheckAll.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPath.Location = new System.Drawing.Point(12, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(523, 20);
+            this.tbPath.TabIndex = 6;
+            this.tbPath.TextChanged += new System.EventHandler(this.tbPath_TextChanged);
+            // 
+            // SelectorLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 346);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnCheckAll);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txParameters);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btnGo);
+            this.Name = "SelectorLauncher";
+            this.Text = "...";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox txParameters;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnCheckAll;
+        private System.Windows.Forms.TextBox tbPath;
+    }
+}
+
---
+++ Bulk-Sequential-Launcher/SelectorLauncher.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Bulk_Sequential_Launcher.Properties;
+
+namespace Bulk_Sequential_Launcher
+{
+    public partial class SelectorLauncher : Form
+    {
+        private bool _refreshing;
+
+        public SelectorLauncher()
+        {
+            InitializeComponent();
+        }
+
+        public string CurrentPath
+        {
+            get
+            {
+                if (string.Equals(tbPath.Text, string.Empty, StringComparison.Ordinal))
+                    tbPath.Text = Application.StartupPath;
+                return tbPath.Text;
+            }
+            set { tbPath.Text = value; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                var processPath = item.ToString();
+                if (File.Exists(processPath))
+                {
+                    var process = Process.Start(processPath, txParameters.Text);
+                    while (process != null && !process.HasExited)
+                        process?.WaitForExit(100);
+                }
+            }
+            if (Resources.FinishedCaption != null)
+                Text = Resources.FinishedCaption;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshFileList();
+        }
+
+        private void RefreshFileList()
+        {
+            if (!_refreshing && Directory.Exists(CurrentPath))
+                try
+                {
+                    _refreshing = true;
+                    checkedListBox1.Items.Clear();
+                    var files = Directory.EnumerateFiles(CurrentPath);
+                    foreach (var file in files)
+                    {
+                        var item = checkedListBox1.Items.Add(file);
+                        checkedListBox1.SetItemChecked(item, file.EndsWith("exe"));
+                    }
+                }
+                finally
+                {
+                    _refreshing = false;
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetAllState(CheckState.Unchecked);
+        }
+
+        private void SetAllState(CheckState checkState)
+        {
+            for (var idx = 0; idx < checkedListBox1.Items.Count; idx++)
+                checkedListBox1.SetItemCheckState(idx, checkState);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetAllState(CheckState.Checked);
+        }
+
+        private void tbPath_TextChanged(object sender, EventArgs e)
+        {
+            RefreshFileList();
+        }
+    }
+}
